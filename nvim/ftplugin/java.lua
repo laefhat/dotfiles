f88fcaf82f33bd25d8682cd556ad47301ef21a3f
Khoa Nguyen <khoa.nguyen@netapp.com>
2025-07-08 21:37:38 +0700

    nvim-jdtls

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -2,3 +2,130 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.expandtab = true
+
+local jdtls = os.getenv("XDG_DATA_HOME") .. "/jdtls"
+local workspace_dir = os.getenv("XDG_CACHE_HOME") .. "/jdtls/workspace/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local config = require("lsp").make_config()
+config.name = "jdtls"
+-- the command that starts the language server
+-- see: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+config.cmd = {
+    os.getenv("JAVASE_21") .. "/bin/java",
+    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+    "-Dosgi.bundles.defaultStartLevel=4",
+    "-Declipse.product=org.eclipse.jdt.ls.core.product",
+    "-Dlog.protocol=true",
+    "-Dlog.level=ALL",
+    "-Xmx2g",
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens", "java.base/java.util=ALL-UNNAMED",
+    "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+    "-jar", jdtls .. "/plugins/org.eclipse.equinox.launcher_1.7.0.v20250331-1702.jar",
+    "-configuration", jdtls .. "/config_mac_arm",
+    "-data", workspace_dir
+}
+-- root_dir = vim.fs.root(0, {".git", "mvnw", "gradlew"}),
+config.root_dir = vim.fn.getcwd()
+
+-- here you can configure eclipse.jdt.ls specific settings
+-- see https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+-- for a list of options
+config.settings = {
+    java = {
+        eclipse = {
+            downloadSources = true,
+        },
+        maven = {
+            downloadSources = true,
+        },
+        signatureHelp = { enabled = true },
+        contentProvider = { preferred = 'fernflower' },
+        completion = {
+            favoriteStaticMembers = {
+                "org.hamcrest.MatcherAssert.assertThat",
+                "org.hamcrest.Matchers.*",
+                "org.hamcrest.CoreMatchers.*",
+                "org.junit.jupiter.api.Assertions.*",
+                "java.util.Objects.requireNonNull",
+                "java.util.Objects.requireNonNullElse",
+                "org.mockito.Mockito.*"
+            },
+            filteredTypes = {
+                "com.sun.*",
+                "io.micrometer.shaded.*",
+                "java.awt.*",
+                "jdk.*",
+                "sun.*",
+            },
+            importOrder = {
+                "java",
+                "javax",
+                "com",
+                "org",
+            },
+        },
+        sources = {
+            organizeImports = {
+                starThreshold = 9999,
+                staticStarThreshold = 9999,
+            },
+        },
+        codeGeneration = {
+            toString = {
+                template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
+            },
+            useBlocks = true,
+        },
+        configuration = {
+            runtimes = {
+                {
+                    name = "JavaSE-11",
+                    path = os.getenv("JAVASE_11"),
+                    default = true
+                },
+                {
+                    name = "JavaSE-17",
+                    path = os.getenv("JAVASE_17"),
+                },
+                {
+                    name = "JavaSE-21",
+                    path = os.getenv("JAVASE_21"),
+                },
+            }
+        }
+    }
+}
+
+-- language server `initializationoptions`
+-- You need to extend the `bundles` with paths to jar files
+-- if you want to use additional eclipse.jdt.ls plugins.
+--
+-- see https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+--
+-- if you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+config.init_options = {
+    extendedClientCapabilities = {
+        classFileContentsSupport = true,
+        generateToStringPromptSupport = true,
+        hashCodeEqualsPromptSupport = true,
+        advancedExtractRefactoringSupport = true,
+        advancedOrganizeImportsSupport = true,
+        generateConstructorsPromptSupport = true,
+        generateDelegateMethodsPromptSupport = true,
+        moveRefactoringSupport = true,
+        overrideMethodsPromptSupport = true,
+        executeClientCommandSupport = true,
+        resolveAdditionalTextEditsSupport = true,
+        inferSelectionSupport = {
+            "extractMethod",
+            "extractVariable",
+            "extractConstant",
+            "extractVariableAllOccurrence"
+        },
+    },
+    bundles = {}
+}
+
+-- this starts a new client & server,
+-- or attaches to an existing client & server depending on the `root_dir`.
+require("jdtls").start_or_attach(config)
--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,44 +1,52 @@
+local M = {}
+
+-- make default config
+function M.make_config()
+    return {
+        on_attach = function(client, bufnr)
+            -- enable inlay hint
+            vim.lsp.inlay_hint.enable(true)
+
+            -- mappings.
+            -- see `:help vim.lsp.*` for documentation on any of the below functions
+            -- lsp navigation keymaps
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr })
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = bufnr })
+
+            -- lsp actions
+            vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { buffer = bufnr })
+            vim.keymap.set("v", "ga", vim.lsp.buf.code_action, { buffer = bufnr })
+            vim.keymap.set("n", "gR", vim.lsp.buf.rename, { buffer = bufnr })
+
+            -- diagnostics keymaps
+            vim.keymap.set("n", "<C-k>", function() vim.diagnostic.jump { count = -1, float = false } end)
+            vim.keymap.set("n", "<C-j>", function() vim.diagnostic.jump { count = 1, float = false } end)
+
+            -- diagnostics list
+            vim.keymap.set("n", "<leader>x", function()
+                vim.diagnostic.setqflist { severity = vim.diagnostic.severity.ERROR }
+            end)
+
+            -- diagnostic signs
+            vim.diagnostic.config { virtual_text = true, underline = true }
+
+            -- completion
+            vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = false })
+        end,
+        detached = true,
+    }
+end
+
 -- default for all servers
-vim.lsp.config("*", {
-    on_attach = function(client, bufnr)
-        -- enable inlay hint
-        vim.lsp.inlay_hint.enable(true)
-
-        -- mappings.
-        -- see `:help vim.lsp.*` for documentation on any of the below functions
-        -- lsp navigation keymaps
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr })
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = bufnr })
-
-        -- lsp actions
-        vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { buffer = bufnr })
-        vim.keymap.set("v", "ga", vim.lsp.buf.code_action, { buffer = bufnr })
-        vim.keymap.set("n", "gR", vim.lsp.buf.rename, { buffer = bufnr })
-
-        -- diagnostics keymaps
-        vim.keymap.set("n", "<C-k>", function() vim.diagnostic.jump { count = -1, float = false } end)
-        vim.keymap.set("n", "<C-j>", function() vim.diagnostic.jump { count = 1, float = false } end)
-
-        -- diagnostics list
-        vim.keymap.set("n", "<leader>x", function()
-            vim.diagnostic.setqflist { severity = vim.diagnostic.severity.ERROR }
-        end)
-
-        -- diagnostic signs
-        vim.diagnostic.config { virtual_text = true, underline = true }
-
-        -- completion
-        vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = false })
-    end,
-    detached = true,
-})
+vim.lsp.config("*", M.make_config())
 
 -- load config for servers
 require("lsp.clangd")
-require("lsp.jdtls")
 require("lsp.luals")
 require("lsp.pyright")
 require("lsp.tsserver")
 
 -- can be disabled by `:lua vim.lsp.enable("tsserver", false)` for example
-vim.lsp.enable({ "clangd", "jdtls", "luals", "pyright", "tsserver" })
+vim.lsp.enable({ "clangd", "luals", "pyright", "tsserver" })
+
+return M
